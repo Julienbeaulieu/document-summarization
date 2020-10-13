4cdf6abed10e9f4af2050b6835ef807c2768ba79
julienbeaulieu <beaulieu.jb@gmail.com>
2020-10-13 16:42:21 -0400

    Arrange requirements files to load only necessary packages for deployment
    Adjust Dockerfile and bash scripts to run requirements-dev.txt
    Remove packages from requirements-dev.in to make it more lightweight

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.7-buster
+
+# Create the working directory
+RUN set -ex && mkdir /repo
+WORKDIR /repo
+
+# Copy only the relevant directories to the working directory
+COPY src/ ./src
+COPY api/ ./api
+COPY .env .
+
+# Install Python dependencies - api/requirements is built in corresponding shell script
+RUN set -ex && pip3 install -r api/requirements-dev.txt
+
+# Run the web server locally - if using Heroku add --server.port $PORT 
+EXPOSE 8000
+ENV PYTHONPATH /repo
+CMD streamlit run /repo/api/streamlit.py 
--- tasks/build_api_docker.sh
+++ tasks/build_api_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install cpu only version of pytorch + all other requirements
-# sed 's/torch==1.6.0/torch==1.6.0' requirements.txt > api/requirements.txt
-cp ~/document-summarization/requirements.txt ~/document-summarization/api
+sed -i '1i--find-links https://download.pytorch.org/whl/torch_stable.html' requirements-dev.txt
+sed 's/torch==1.6.0/torch==1.6.0+cpu/' requirements-dev.txt > api/requirements-dev.txt
 
-docker build -t text_summarizer_api -f api/Dockerfile .
+docker build -t text_summarizer_api -f Dockerfile . 
